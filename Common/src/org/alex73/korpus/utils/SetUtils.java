e2b7b65ebcb71da9698b62f336aee456413ec779
Aleś Bułojčyk <alex73mail@gmail.com>
2018-05-22 09:20:44 +0300

    Set utils

--- Common/src/org/alex73/korpus/utils/SetUtils.java
+++ Common/src/org/alex73/korpus/utils/SetUtils.java
@@ -2,6 +2,10 @@ package org.alex73.korpus.utils;
 
 import java.util.Set;
 
+import org.alex73.corpus.paradigm.Form;
+import org.alex73.corpus.paradigm.Paradigm;
+import org.alex73.corpus.paradigm.Variant;
+
 public class SetUtils {
 
     public static String set2string(Set<String> set) {
@@ -16,5 +20,55 @@ public class SetUtils {
             r.append(s);
         }
         return r.toString();
+    }
+
+    public static String addTag(String orig, String tag) {
+        if (orig == null || orig.trim().isEmpty()) {
+            return tag;
+        }
+        if (hasTag(orig, tag)) {
+            return orig;
+        }
+        return orig + ',' + tag;
+    }
+
+    public static boolean hasTag(String orig, String tag) {
+        if (orig == null || orig.trim().isEmpty()) {
+            return false;
+        }
+        for (String v : orig.split(",")) {
+            if (v.trim().equals(tag)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasSlounik(Form f, String slounik) {
+        return hasTag(f.getSlouniki(), slounik);
+    }
+
+    public static void addSlounik(Form f, String slounik) {
+        f.setSlouniki(addTag(f.getSlouniki(), slounik));
+    }
+
+    public static boolean hasPravapis(Form f, String pravapis) {
+        return hasTag(f.getPravapis(), pravapis);
+    }
+
+    public static boolean hasPravapis(Variant v, String pravapis) {
+        return hasTag(v.getPravapis(), pravapis);
+    }
+
+    public static void addPravapis(Form f, String pravapis) {
+        f.setPravapis(addTag(f.getPravapis(), pravapis));
+    }
+
+    public static void addPravapis(Variant v, String pravapis) {
+        v.setPravapis(addTag(v.getPravapis(), pravapis));
+    }
+
+    public static String toString(Paradigm p) {
+        return "Paradigm: " + p.getTag() + "/" + p.getLemma() + "[" + p.getPdgId() + "]";
     }
 }
