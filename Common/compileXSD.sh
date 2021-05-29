e34472093a06cd05edc153dd872a9e4c909d26b3
Aleś Bułojčyk <alex73mail@gmail.com>
2021-05-29 09:17:30 +0300

    Remove old data structures

--- Common/compileXSD.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/opt/java/bin/xjc -no-header -d src -p org.alex73.corpus.paradigm src/xsd/Paradigm.xsd
--- Common/src/org/alex73/korpus/text/xml/ITextLineElement.java
+++
@@ -1,5 +0,0 @@
-package org.alex73.korpus.text.xml;
-
-public interface ITextLineElement {
-    String getText();
-}
