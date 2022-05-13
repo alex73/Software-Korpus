cb73c3dfd6c30bb0e029b67e491411968b8de9be
Aleś Bułojčyk <alex73mail@gmail.com>
2022-05-13 11:34:49 +0300

    updates

--- Web/src/org/alex73/korpus/server/html/MakeHTML.java
+++ Web/src/org/alex73/korpus/server/html/MakeHTML.java
@@ -16,6 +16,7 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.TemplateNotFoundException;
 
 @SuppressWarnings("serial")
 @WebServlet(name="HTML", urlPatterns = { "*.html" })
@@ -27,25 +28,24 @@ public class MakeHTML extends HttpServlet {
         super.init(config);
         cfg = new Configuration(Configuration.VERSION_2_3_23);
         cfg.setServletContextForTemplateLoading(getServletContext(), "/WEB-INF/templates/");
-//        try {
-//            cfg.setDirectoryForTemplateLoading(new File(System.getProperty("CONFIG_DIR")));
-//        } catch (IOException ex) {
-//            throw new ServletException(ex);
-//        }
         cfg.setDefaultEncoding("UTF-8");
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Template t = cfg.getTemplate(req.getServletPath());
-        Map<String, Object> context = new TreeMap<>();
-        resp.setContentType("text/html; charset=UTF-8");
-        try (OutputStreamWriter wr = new OutputStreamWriter(resp.getOutputStream(), "UTF-8")) {
-            t.process(context, wr);
-        } catch (TemplateException ex) {
-            ex.printStackTrace();
-            throw new ServletException(ex);
+        try {
+            Template t = cfg.getTemplate(req.getServletPath());
+            Map<String, Object> context = new TreeMap<>();
+            resp.setContentType("text/html; charset=UTF-8");
+            try (OutputStreamWriter wr = new OutputStreamWriter(resp.getOutputStream(), "UTF-8")) {
+                t.process(context, wr);
+            } catch (TemplateException ex) {
+                ex.printStackTrace();
+                throw new ServletException(ex);
+            }
+        } catch (TemplateNotFoundException ex) {
+            resp.sendError(404);
         }
     }
 }
