d4ec027505ac98921330ef8d3041fc73c5a05ceb
Aleś Bułojčyk <alex73mail@gmail.com>
2022-09-23 16:00:50 +0300

    Націскі

--- Web/src/org/alex73/korpus/future/Kanvertary.java
+++ Web/src/org/alex73/korpus/future/Kanvertary.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 import org.alex73.corpus.paradigm.Form;
 import org.alex73.corpus.paradigm.Paradigm;
 import org.alex73.corpus.paradigm.Variant;
+import org.alex73.fanetyka.impl.FanetykaText;
 import org.alex73.korpus.belarusian.BelarusianWordNormalizer;
 import org.alex73.korpus.server.KorpusApplication;
 import org.alex73.korpus.text.parser.IProcess;
@@ -22,13 +23,33 @@ import org.alex73.korpus.text.structure.files.TextLine;
 import org.alex73.korpus.text.structure.files.WordItem;
 import org.alex73.korpus.utils.StressUtils;
 
-@Path("/naciski")
-public class Naciski {
+@Path("/conv")
+public class Kanvertary {
 
     @POST
+    @Path("fanetyka")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces("text/html; charset=UTF-8")
+    public String fanetyka(String text) throws Exception {
+        try {
+            if (text.isBlank()) {
+                throw new Exception("Пусты тэкст");
+            }
+
+            FanetykaText f = new FanetykaText(KorpusApplication.instance.grFinder, text.replace('\u0301', '+').replace('+', '´').replaceAll("[-‒‒–]", "-"));
+
+            return "<div>Вынікі канвертавання (IPA):</div><div style='font-size: 150%'>" + f.ipa.replace("\n", "<br/>")
+                    + "</div><br/><div>Школьная транскрыпцыя:</div><div style='font-size: 150%'>" + f.skola.replace("\n", "<br/>") + "</div>";
+        } catch (Exception ex) {
+            return "Памылка: " + ex.getMessage();
+        }
+    }
+
+    @POST
+    @Path("naciski")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces("text/plain; charset=UTF-8")
-    public String search(String text) throws Exception {
+    public String naciski(String text) throws Exception {
         Splitter3 splitter = new Splitter3(false, new IProcess() {
             @Override
             public synchronized void showStatus(String status) {
