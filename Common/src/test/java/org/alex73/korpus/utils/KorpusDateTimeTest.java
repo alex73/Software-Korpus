0a752b4b9d47aeffd3a21b188e30944978e2d47f
Aleś Bułojčyk <alex73mail@gmail.com>
2025-11-30 08:20:20 +0300

    Change ant->mvn

---
+++ Common/src/test/java/org/alex73/korpus/utils/KorpusDateTimeTest.java
@@ -0,0 +1,15 @@
+package org.alex73.korpus.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class KorpusDateTimeTest {
+    @Test
+    public void testXmlDateTime() {
+        new KorpusDateTime("2015-03-17");
+        new KorpusDateTime("2015-03-17T16:30:38");
+        new KorpusDateTime("2015-03-17T16:30:38+00:00");
+        new KorpusDateTime("07.05.1994-31.08.1996");
+        new KorpusDateTime("2015?");
+        new KorpusDateTime("[2015]");
+    }
+}
